abcf818b6bde360359112c8cd4d3316005d28fd1
Matthew Stratton <matthew.m.stratt@gmail.com>
2018-08-04 18:17:06 -0400

    basic alexandria cli

---
+++ alexandria-cli/src/main/java/com/github/macgregor/alexandria/Application.java
@@ -0,0 +1,56 @@
+package com.github.macgregor.alexandria;
+
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.concurrent.Callable;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+@CommandLine.Command(
+        description = "Command line interface for converting markdown files into html and publish them",
+        name = "alexandria",
+        mixinStandardHelpOptions = true,
+        versionProvider = Application.ManifestVersionProvider.class,
+        subcommands = {
+            ConvertCommand.class
+        })
+public class Application implements Callable<Void> {
+
+    public static void main(String[] args) {
+        CommandLine cmd = new CommandLine(new Application());
+        cmd.parseWithHandler(new CommandLine.RunLast(), System.err, args);
+    }
+
+    @Override
+    public Void call() throws Exception {
+        return null;
+    }
+
+    static class ManifestVersionProvider implements CommandLine.IVersionProvider {
+        public String[] getVersion() throws Exception {
+            Enumeration<URL> resources = CommandLine.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                try {
+                    Manifest manifest = new Manifest(url.openStream());
+                    if (isApplicableManifest(manifest)) {
+                        Attributes attributes = manifest.getMainAttributes();
+                        return new String[] { attributes.get(key("Implementation-Title")) + " version \"" + attributes.get(key("Implementation-Version")) + "\"" };
+                    }
+                } catch (IOException ex) {
+                    return new String[] { "Unable to read from " + url + ": " + ex };
+                }
+            }
+            return new String[0];
+        }
+
+        private boolean isApplicableManifest(Manifest manifest) {
+            Attributes attributes = manifest.getMainAttributes();
+            return "alexandria-cli".equals(attributes.get(key("Implementation-Title")));
+        }
+        private static Attributes.Name key(String key) { return new Attributes.Name(key); }
+    }
+}
