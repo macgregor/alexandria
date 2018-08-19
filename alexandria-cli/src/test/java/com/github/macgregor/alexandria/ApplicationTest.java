ac7d8eed6bccf376d109cfd55202ca5ebaf0de6e
Matthew Stratton <mstratto@redhat.com>
2018-08-19 19:37:16 -0400

    improving test coverage

---
+++ alexandria-cli/src/test/java/com/github/macgregor/alexandria/ApplicationTest.java
@@ -0,0 +1,43 @@
+package com.github.macgregor.alexandria;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void testSyncIsCalled() throws Exception {
+        Application application = spy(new Application());
+        Alexandria alexandria = alexandriaSpy(application);
+        application.call();
+        verify(alexandria, times(1)).syncWithRemote();
+    }
+
+    @Test
+    public void testIndexIsCalled() throws Exception {
+        Application application = spy(new Application());
+        Alexandria alexandria = alexandriaSpy(application);
+        application.call();
+        verify(alexandria, times(1)).index();
+    }
+
+    @Test
+    public void testConvertIsCalled() throws Exception {
+        Application application = spy(new Application());
+        Alexandria alexandria = alexandriaSpy(application);
+        application.call();
+        verify(alexandria, times(1)).convert();
+    }
+
+    private Alexandria alexandriaSpy(Application application) throws Exception {
+        Alexandria alexandria = spy(new Alexandria());
+        Context context = spy(new Context());
+        alexandria.context(context);
+        doReturn(alexandria).when(alexandria).index();
+        doReturn(alexandria).when(alexandria).convert();
+        doReturn(alexandria).when(alexandria).syncWithRemote();
+        doReturn(alexandria).when(application).getAlexandria();
+        return alexandria;
+    }
+}
