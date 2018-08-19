ec71a4c454562016aa16e7ddb6cf379f0d374946
Matthew Stratton <matthew.m.stratt@gmail.com>
2018-08-19 19:37:16 -0400

    improving test coverage

---
+++ alexandria-cli/src/test/java/com/github/macgregor/alexandria/IndexCommandTest.java
@@ -0,0 +1,22 @@
+package com.github.macgregor.alexandria;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class IndexCommandTest {
+
+    @Test
+    public void testIndexIsCalled() throws Exception {
+        IndexCommand testCommand = spy(new IndexCommand());
+        Alexandria alexandria = spy(new Alexandria());
+        Context context = spy(new Context());
+        alexandria.context(context);
+        doReturn(alexandria).when(alexandria).index();
+        doReturn(alexandria).when(alexandria).convert();
+        doReturn(alexandria).when(alexandria).syncWithRemote();
+        doReturn(alexandria).when(testCommand).getAlexandria();
+        testCommand.call();
+        verify(alexandria, times(1)).index();
+    }
+}
---
+++ alexandria-cli/src/test/java/com/github/macgregor/alexandria/SyncCommandTest.java
@@ -0,0 +1,22 @@
+package com.github.macgregor.alexandria;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class SyncCommandTest {
+
+    @Test
+    public void testSyncIsCalled() throws Exception {
+        SyncCommand testCommand = spy(new SyncCommand());
+        Alexandria alexandria = spy(new Alexandria());
+        Context context = spy(new Context());
+        alexandria.context(context);
+        doReturn(alexandria).when(alexandria).index();
+        doReturn(alexandria).when(alexandria).convert();
+        doReturn(alexandria).when(alexandria).syncWithRemote();
+        doReturn(alexandria).when(testCommand).getAlexandria();
+        testCommand.call();
+        verify(alexandria, times(1)).syncWithRemote();
+    }
+}
